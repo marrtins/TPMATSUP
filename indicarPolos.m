cb412867e328a2c9b108c3e990d96f9f150fc4d1
gerosamore <cortigeronimo@gmail.com>
2017-06-02 00:28:55 -0300

    cree la ventana para indicar los polos de una funcion de transferencia

--- indicarPolos.m
+++ indicarPolos.m
@@ -1,4 +1,23 @@
-function polos = indicarPolos(funcionTransferencia)
+function indicarPolos(funcionTransferencia)
+
+altoVariable = 370; 
 denominador = funcionTransferencia.den{1};
 polos = roots(denominador);
+
+
+h = dialog("name", "Indicador de Polos");
+botonVolverAlMenuPrincipal(h, [400 20 100 20]);
+botonCerrarVentana(h, [330 20 50 20]);
+uicontrol(h, "style", "text", "string", "Estos son los Polos de la funcion:", "position",[20 400 250 10], "horizontalalignment", "left");
+
+
+for i = 0:length(polos)-1
+	c = i + 1;
+	string = strcat("polo ", num2str(c));
+	string = strcat(string, ":  ");
+	string = strcat(string, num2str(polos(c)));
+	uicontrol(h, "style", "text", "string", string, "position",[20 altoVariable 150 15], "horizontalalignment", "left");
+	altoVariable = altoVariable - 20;
+end
+
 endfunction
