118d11fda5210df63af780e1c919b7fbce689450
gerosamore <cortigeronimo@gmail.com>
2017-06-01 23:46:33 -0300

    cree la ventana que muestra los ceros de una funcion de transferencia

--- indicarCeros.m
+++ indicarCeros.m
@@ -1,4 +1,18 @@
-function ceros = indicarCeros(funcionTransferencia)
-numerador = funcionTransferencia.num{1};
-ceros = roots(numerador);
+function indicarCeros(funcionTransferencia)
+altoVariable = 370; %altura que va a ir variando
+numerador = funcionTransferencia.num{1}; %tomo el numerador de la funcion de transferencia
+ceros = roots(numerador); %tomo los ceros de la funcion
+h = dialog("name", "Indicador de Ceros");
+uicontrol(h, "style", "text", "string", "Estos son los Ceros de la funcion:", "position",[20 400 250 10], "horizontalalignment", "left");
+for i = 0:length(ceros)-1 %desde cero a n-1
+	c = i + 1;
+	string = strcat("cero ", num2str(c)); %me quedaría (en la primera vuelta) "cero 1"
+	string = strcat(string, ":  "); %me quedaría (en la primera vuelta) "cero 1: "
+	string = strcat(string, num2str(ceros(c))); %me quedaria (en la primera vuelta) "cero1: valorDelCero"
+	%lo que sigue, genera los labels, y por cada vuelta le resta 20 a la altura, para que quede uno abajo del otro
+	%le puse la propiedad "horizontalalignment" con valor "left" para que todos queden alineados a izquierda
+	uicontrol(h, "style", "text", "string", string, "position",[20 altoVariable 150 10], "horizontalalignment", "left");
+	altoVariable = altoVariable - 20;
+end
+
 endfunction
