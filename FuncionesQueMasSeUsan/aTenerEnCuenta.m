b5a0a81e04c55d017a921f1d63a79ade8680175f
carogon <carolina.malen.gonzalez@gmail.com>
2017-06-04 16:27:20 -0300

    agregue algunas cosas al archivo de aTenerEnCuenta

--- FuncionesQueMasSeUsan/aTenerEnCuenta.m
+++ FuncionesQueMasSeUsan/aTenerEnCuenta.m
@@ -1,6 +1,6 @@
 % con el simbolo de porcentaje se comenta
 %IMPORTANTE: el nombre del archivo tienen que tener el mismo nombre que el de la funcion, sino el archivo no funciona
-%así se define una función void, que no recibe parametros
+%asi se define una funcion void, que no recibe parametros
 function prueba() 
 
 % asi se define una funcion que devuelve "resultado" y recibe parametros
@@ -17,21 +17,25 @@ h = dialog('name','nombre de la ventana');
 %como dije, los parametros del uicontrol son: primero va el puntero a la ventana donde quiero crear el elemento
 %despues, los parametros vienen de a pares 'parametro', 'valor', 'parametro2', 'valor2'
 %en este caso, el primer parametro es el puntero a la ventana que cree
-$el primer PAR de parametros es 'style', 'pushbutton' indica que el estilo del elemento es un botton apretable
+%el primer PAR de parametros es 'style', 'pushbutton' indica que el estilo del elemento es un botton apretable
 %el segundo par de parametros es 'string', 'esto es un boton' que indica el string que se va a mostrar en el boton
 %el ultimo par (quizas el mas importante) indica la posicion donde va a estar el elemento dentro de la ventana
 %las ventanas son de 400 de alto, por 500 de largo (para que lo tengan en cuenta)
-%el valor de 'position' en este caso es [10 400 100 20] que significa [posicionEnX posicionEnY largoDelElemento altoDelElemento]
-b1 = uicontrol(h, 'style', 'pushbutton', 'string', 'esto es un boton', 'position', [10 400 100 20]);
+%el valor de 'position' en este caso es [10 400 150 20] que significa [posicionEnX posicionEnY largoDelElemento altoDelElemento]
+b1 = uicontrol(h, 'style', 'pushbutton', 'string', 'esto es un boton', 'position', [10 400 150 20]);
 
 %ejemplo de mostrar un texto
-b2 = uicontrol(h, 'style', 'text', 'string', 'esto es un texto a mostrar', 'position', [10 370 100 20]);
+b2 = uicontrol(h, 'style', 'text', 'string', 'esto es un texto a mostrar', 'position', [10 370 150 20]);
 
 %ejemplo de campo de texto
-b3 = uicontrol(h, 'style', 'edit', 'position', [10 340 100 20]);
+b3 = uicontrol(h, 'style', 'edit', 'position', [10 340 150 20]);
 
 %el boton apretable que cree anteriormente no hace nada. para que cuando lo aprete, genere alguna funcionalidad
-%tengo que usar la propiedad callback, y decirle que funciones se van a ejecutar
+%tengo que usar la propiedad callback (lo que hace es llamar a la/s funcion/es una vez que ocurre un evento, en este caso apretar el bot�n), y decirle que funciones se van a ejecutar
 %delete(gcf) cierra la ventana actual
-b4 = uicontrol(h, 'style', 'pushbutton', 'string', 'boton con funcionalidad', 'position', [10 300 120 20], 'callback', 'delete(gcf)')
+b4 = uicontrol(h, 'style', 'pushbutton', 'string', 'boton con funcionalidad', 'position', [10 300 150 20], 'callback', 'delete(gcf)')
 
+%La funcion siempre termina con un endfunction
+endfunction
+
+%Si quieren pueden ejecutar este archivo para ver como muestra todos los elementos
