e5f67f8e0d07cbd1613961ffb994058addd92a70
carogon <carolina.malen.gonzalez@gmail.com>
2017-06-06 16:03:14 -0300

    saco el boton de ir al menu principal de las funcionalidades porque si no no cierra la ventana

--- mostrarFuncionTransferencia.m
+++ mostrarFuncionTransferencia.m
@@ -1,8 +1,8 @@
 function mostrarFuncionTransferencia(funcionTransferencia)
   
 h = dialog("name", "Función Transferencia");
-botonVolverAlMenuPrincipal(h, 400 ,20);
-botonCerrarVentana(h, 300, 20);
+
+botonCerrarVentana(h, 400, 20);
 
 f = evalc('funcionTransferencia');
 uicontrol(h, "style", "text", "string", f, "position",[10 250 400 150], "horizontalalignment", "left");
