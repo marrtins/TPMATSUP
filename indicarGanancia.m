9eb458596db84466a591dd90a2a1a0c7715cf3d2
gerosamore <cortigeronimo@gmail.com>
2017-06-07 22:52:32 -0300

    conecto lo que hizo enzo al menu de funcionalidades

--- indicarGanancia.m
+++ indicarGanancia.m
@@ -7,6 +7,7 @@ uicontrol(h, "style", "text", "string", "Esta es la Ganancia de la funcion:", "p
 
 [z,p,k] = tf2zp(funcionTransferencia);
 ganancia = num2str(k);
+ganancia = strcat("k:", ganancia);
 uicontrol(h, "style", "text", "string", ganancia, "position",[20 380 250 10], "horizontalalignment", "left");
 
 endfunction
